96148ef733315db32fde84b32cdaa64666906e0d
Roberto Lo Giacco <rlogiacco@gmail.com>
2015-04-25 15:01:01 +0200

    improved javadocs and test coverage

--- src/main/java/org/agileware/test/web/WaitHelper.java
+++ src/main/java/org/agileware/test/web/WaitHelper.java
@@ -16,6 +16,7 @@ package org.agileware.test.web;
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.hamcrest.Matcher;
@@ -28,8 +29,27 @@ import org.openqa.selenium.support.ui.FluentWait;
 import com.google.common.base.Function;
 
 /**
+ * This helper class provides a few utility methods really helpful in testing
+ * asynchronous web applications like those made with AngularJS or jQuery.
  * 
- *
+ * <p>
+ * Many JavaScript frameworks operate by manipulating the page DOM either by
+ * adding/removing or by showing/hiding elements as a consequence of a user
+ * action: this implies your test code needs to synchronize with those
+ * asynchronous events in order to proceed.
+ * </p>
+ * 
+ * <p>
+ * The utility methods presented in here provide the basic facility to create
+ * this synchronization points.
+ * </p>
+ * 
+ * <p>
+ * More advanced synchronization than presented here is achievable by directly
+ * using the <code>org.openqa.selenium.support.ui.Wait</code> or
+ * <code>org.openqa.selenium.support.ui.FluentWait</code> APIs and the code
+ * provided hereby can be used as a reference for such advanced usage.
+ * </p>
  */
 public class WaitHelper {
 
@@ -39,8 +59,12 @@ public class WaitHelper {
 
 	/**
 	 * @param context
+	 *            the <code>WebDriver</code> or <code>WebElement</code> used as
+	 *            search root
 	 * @param duration
+	 *            the maximum wait duration
 	 * @param timeUnit
+	 *            the maximum wait time unit
 	 */
 	protected WaitHelper(SearchContext context, long duration, TimeUnit timeUnit) {
 		this.context = context;
@@ -50,8 +74,12 @@ public class WaitHelper {
 
 	/**
 	 * @param context
+	 *            the <code>WebDriver</code> or <code>WebElement</code> used as
+	 *            search root
 	 * @param millis
-	 * @return
+	 *            the maximum wait in milliseconds
+	 * @return a <code>WaitHelper</code> instance operating on the above
+	 *         parameters
 	 */
 	public static WaitHelper waitOn(SearchContext context, long millis) {
 		return waitOn(context, millis, TimeUnit.MILLISECONDS);
@@ -59,20 +87,32 @@ public class WaitHelper {
 
 	/**
 	 * @param context
+	 *            the <code>WebDriver</code> or <code>WebElement</code> used as
+	 *            search root
 	 * @param duration
+	 *            the maximum wait duration
 	 * @param timeUnit
-	 * @return
+	 *            the maximum wait time unit
+	 * @return a <code>WaitHelper</code> instance operating on the above
+	 *         parameters
 	 */
 	public static WaitHelper waitOn(SearchContext context, long duration, TimeUnit timeUnit) {
 		return new WaitHelper(context, duration, timeUnit);
 	}
 
 	/**
+	 * Holds the execution until an element matching the search condition is
+	 * <b>found</b> or the maximum wait expires.
+	 * 
 	 * @param by
-	 * @return
+	 *            search condition
+	 * @return the found element
 	 */
 	public WebElement untilAdded(final By by) {
-		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+		return new FluentWait<SearchContext>(context)
+				.withTimeout(duration, timeUnit)
+				.ignoring(NoSuchElementException.class)
+				.until(new Function<SearchContext, WebElement>() {
 			public WebElement apply(SearchContext context) {
 				return context.findElement(by);
 			}
@@ -80,11 +120,22 @@ public class WaitHelper {
 	}
 
 	/**
+	 * Holds the execution until <b>no</b> element matching the search condition is
+	 * <b>found</b> or the maximum wait expires.
+	 * 
+	 * WARNING: the return value of this method might be <code>null</code> and,
+	 * even when it is not, it represents a stale node.
+	 * 
 	 * @param by
-	 * @return
+	 *            search condition
+	 * @return the last known version of the element or <code>null</code> if the
+	 *         element was never found
 	 */
 	public WebElement untilRemoved(final By by) {
-		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+		return new FluentWait<SearchContext>(context)
+				.withTimeout(duration, timeUnit)
+				.ignoring(NoSuchElementException.class)
+				.until(new Function<SearchContext, WebElement>() {
 			private WebElement result;
 
 			public WebElement apply(SearchContext context) {
@@ -99,11 +150,18 @@ public class WaitHelper {
 	}
 
 	/**
+	 * Holds the execution until an element matching the search condition is
+	 * <b>found and visible</b> or the maximum wait expires.
+	 * 
 	 * @param by
-	 * @return
+	 *            search condition
+	 * @return the found element
 	 */
 	public WebElement untilShown(final By by) {
-		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+		return new FluentWait<SearchContext>(context)
+				.withTimeout(duration, timeUnit)
+				.ignoring(NoSuchElementException.class)
+				.until(new Function<SearchContext, WebElement>() {
 			public WebElement apply(SearchContext context) {
 				WebElement result = context.findElement(by);
 				if (!result.isDisplayed()) {
@@ -115,11 +173,18 @@ public class WaitHelper {
 	}
 
 	/**
+	 * Holds the execution until an element matching the search condition is
+	 * <b>found and non visible</b> or <b>not found</b> or the maximum wait expires.
+	 * 
 	 * @param by
-	 * @return
+	 *            search condition
+	 * @return the found element
 	 */
 	public WebElement untilHidden(final By by) {
-		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+		return new FluentWait<SearchContext>(context)
+				.withTimeout(duration, timeUnit)
+				.ignoring(NoSuchElementException.class)
+				.until(new Function<SearchContext, WebElement>() {
 			public WebElement apply(SearchContext context) {
 				WebElement result = context.findElement(by);
 				if (result.isDisplayed()) {
@@ -131,11 +196,18 @@ public class WaitHelper {
 	}
 
 	/**
+	 * Holds the execution until an element matching the search condition is
+	 * <b>found and enabled</b> or the maximum wait expires.
+	 * 
 	 * @param by
-	 * @return
+	 *            search condition
+	 * @return the found element
 	 */
 	public WebElement untilEnabled(final By by) {
-		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+		return new FluentWait<SearchContext>(context)
+				.withTimeout(duration, timeUnit)
+				.ignoring(NoSuchElementException.class)
+				.until(new Function<SearchContext, WebElement>() {
 			public WebElement apply(SearchContext context) {
 				WebElement result = context.findElement(by);
 				if (!result.isEnabled()) {
@@ -147,8 +219,12 @@ public class WaitHelper {
 	}
 
 	/**
+	 * Holds the execution until an element matching the search condition is
+	 * <b>found and enabled</b> or <b>not found</b> or the maximum wait expires.
+	 * 
 	 * @param by
-	 * @return
+	 *            search condition
+	 * @return the found element
 	 */
 	public WebElement untilDisabled(final By by) {
 		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
@@ -163,15 +239,56 @@ public class WaitHelper {
 	}
 
 	/**
+	 * Holds the execution until an element matching the search condition is
+	 * <b>found and the generic matcher is verified</b> or <b>not found</b> or
+	 * the maximum wait expires.
+	 * 
+	 * @see org.openqa.selenium.lift.Matchers
+	 * @see org.hamcrest.Matcher
+	 * 
 	 * @param by
+	 *            search condition
 	 * @param matcher
-	 * @return
+	 *            the matching condition to check
+	 * @return the found element
 	 */
 	public WebElement until(final By by, final Matcher<WebElement> matcher) {
-		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+		return new FluentWait<SearchContext>(context)
+				.withTimeout(duration, timeUnit)
+				.ignoring(NoSuchElementException.class)
+				.until(new Function<SearchContext, WebElement>() {
 			public WebElement apply(SearchContext context) {
 				WebElement result = context.findElement(by);
 				if (!matcher.matches(result)) {
+					throw new NoSuchElementException("Element does not match");
+				}
+				return result;
+			}
+		});
+	}
+	
+	/**
+	 * Holds the execution until at the elements count matching the search
+	 * condition <b>matches with the generic matcher condition</b> or the
+	 * maximum wait expires.
+	 * 
+	 * @see org.openqa.selenium.lift.Matchers
+	 * @see org.hamcrest.Matcher
+	 * 
+	 * @param by
+	 *            search condition
+	 * @param matcher
+	 *            the matching condition to check
+	 * @return a <code>java.util.List</code> containing found elements
+	 */
+	public List<WebElement> untilCount(final By by, final Matcher<Integer> matcher) {
+		return new FluentWait<SearchContext>(context)
+				.withTimeout(duration, timeUnit)
+				.ignoring(NoSuchElementException.class)
+				.until(new Function<SearchContext, List<WebElement>>() {
+			public List<WebElement> apply(SearchContext context) {
+				List<WebElement> result = context.findElements(by);
+				if (!matcher.matches(result.size())) {
 					throw new NoSuchElementException("Element does not match");
 				}
 				return result;
