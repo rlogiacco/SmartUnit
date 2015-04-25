01b28f4096888c2df9abb124c56a80291d400ec4
Roberto Lo Giacco <rlogiacco@gmail.com>
2015-04-25 02:30:37 +0200

    improved WaitHelper API

--- src/main/java/org/agileware/test/web/WaitHelper.java
+++ src/main/java/org/agileware/test/web/WaitHelper.java
@@ -1,114 +1,180 @@
 package org.agileware.test.web;
 
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 import java.util.concurrent.TimeUnit;
 
+import org.hamcrest.Matcher;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.FluentWait;
 
-import com.google.common.base.Predicate;
+import com.google.common.base.Function;
 
+/**
+ * 
+ *
+ */
 public class WaitHelper {
 
-	private WebDriver driver;
+	private SearchContext context;
 	private long duration;
 	private TimeUnit timeUnit;
 
-	public WaitHelper(WebDriver driver, long millis) {
-		this(driver, millis, TimeUnit.MILLISECONDS);
-	}
-
-	public WaitHelper(WebDriver driver, long duration, TimeUnit timeUnit) {
-		this.driver = driver;
+	/**
+	 * @param context
+	 * @param duration
+	 * @param timeUnit
+	 */
+	protected WaitHelper(SearchContext context, long duration, TimeUnit timeUnit) {
+		this.context = context;
 		this.duration = duration;
 		this.timeUnit = timeUnit;
 	}
 
 	/**
-	 * @param by
+	 * @param context
+	 * @param millis
+	 * @return
 	 */
-	public void untilAdded(final By by, long duration, TimeUnit timeUnit) {
-		this.untilAdded(by, duration, timeUnit);
+	public static WaitHelper waitOn(SearchContext context, long millis) {
+		return waitOn(context, millis, TimeUnit.MILLISECONDS);
+	}
+
+	/**
+	 * @param context
+	 * @param duration
+	 * @param timeUnit
+	 * @return
+	 */
+	public static WaitHelper waitOn(SearchContext context, long duration, TimeUnit timeUnit) {
+		return new WaitHelper(context, duration, timeUnit);
 	}
 
 	/**
 	 * @param by
+	 * @return
 	 */
-	public void untilAdded(final By by) {
-		new FluentWait<WebDriver>(driver).withTimeout(duration, timeUnit).until(new Predicate<WebDriver>() {
-			public boolean apply(WebDriver browser) {
+	public WebElement untilAdded(final By by) {
+		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+			public WebElement apply(SearchContext context) {
+				return context.findElement(by);
+			}
+		});
+	}
+
+	/**
+	 * @param by
+	 * @return
+	 */
+	public WebElement untilRemoved(final By by) {
+		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+			private WebElement result;
+
+			public WebElement apply(SearchContext context) {
 				try {
-					browser.findElement(by);
-					return true;
+					result = context.findElement(by);
 				} catch (NoSuchElementException nsee) {
-					return false;
+					return result;
 				}
+				throw new NoSuchElementException("Element found");
 			}
 		});
 	}
 
 	/**
 	 * @param by
+	 * @return
 	 */
-	public void untilRemoved(final By by) {
-		this.untilRemoved(by, duration, timeUnit);
-	}
-
-	/**
-	 * @param by
-	 * @param duration
-	 * @param timeUnit
-	 */
-	public void untilRemoved(final By by, long duration, TimeUnit timeUnit) {
-		new FluentWait<WebDriver>(driver).withTimeout(duration, timeUnit).until(new Predicate<WebDriver>() {
-			public boolean apply(WebDriver browser) {
-				try {
-					browser.findElement(by);
-					return false;
-				} catch (NoSuchElementException nsee) {
-					return true;
+	public WebElement untilShown(final By by) {
+		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+			public WebElement apply(SearchContext context) {
+				WebElement result = context.findElement(by);
+				if (!result.isDisplayed()) {
+					throw new NoSuchElementException("Element not visible");
 				}
+				return result;
 			}
 		});
 	}
 
 	/**
 	 * @param by
+	 * @return
 	 */
-	public void untilShown(final By by) {
-		this.untilShown(by, duration, timeUnit);
-	}
-
-	/**
-	 * @param by
-	 * @param duration
-	 * @param timeUnit
-	 */
-	public void untilShown(final By by, long duration, TimeUnit timeUnit) {
-		new FluentWait<WebDriver>(driver).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Predicate<WebDriver>() {
-			public boolean apply(WebDriver browser) {
-				return browser.findElement(by).isDisplayed();
+	public WebElement untilHidden(final By by) {
+		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+			public WebElement apply(SearchContext context) {
+				WebElement result = context.findElement(by);
+				if (result.isDisplayed()) {
+					throw new NoSuchElementException("Element is visible");
+				}
+				return result;
 			}
 		});
 	}
 
 	/**
 	 * @param by
+	 * @return
 	 */
-	public void untilHidden(final By by) {
-		this.untilHidden(by, duration, timeUnit);
+	public WebElement untilEnabled(final By by) {
+		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+			public WebElement apply(SearchContext context) {
+				WebElement result = context.findElement(by);
+				if (!result.isEnabled()) {
+					throw new NoSuchElementException("Element not enabled");
+				}
+				return result;
+			}
+		});
 	}
 
 	/**
 	 * @param by
-	 * @param duration
-	 * @param timeUnit
+	 * @return
 	 */
-	public void untilHidden(final By by, long duration, TimeUnit timeUnit) {
-		new FluentWait<WebDriver>(driver).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Predicate<WebDriver>() {
-			public boolean apply(WebDriver browser) {
-				return !browser.findElement(by).isDisplayed();
+	public WebElement untilDisabled(final By by) {
+		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+			public WebElement apply(SearchContext context) {
+				WebElement result = context.findElement(by);
+				if (result.isEnabled()) {
+					throw new NoSuchElementException("Element is enabled");
+				}
+				return result;
+			}
+		});
+	}
+
+	/**
+	 * @param by
+	 * @param matcher
+	 * @return
+	 */
+	public WebElement until(final By by, final Matcher<WebElement> matcher) {
+		return new FluentWait<SearchContext>(context).withTimeout(duration, timeUnit).ignoring(NoSuchElementException.class).until(new Function<SearchContext, WebElement>() {
+			public WebElement apply(SearchContext context) {
+				WebElement result = context.findElement(by);
+				if (!matcher.matches(result)) {
+					throw new NoSuchElementException("Element does not match");
+				}
+				return result;
 			}
 		});
 	}
---
+++ src/test/java/org/agileware/test/web/WaitHelperChromeIT.java
@@ -0,0 +1,93 @@
+package org.agileware.test.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.agileware.test.web.WaitHelper.waitOn;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class WaitHelperChromeIT {
+	private WebDriver browser;
+
+	@Before
+	public void before() {
+		System.setProperty(SharedWebDriver.SELENIUM_DRIVER_PROPERTY, ChromeDriver.class.getName());
+		browser = SharedWebDriver.open();
+	}
+
+	@Test
+	public void testUntilAdded() {
+		browser.get(SharedWebDriverTest.TEST_PAGE);
+		browser.findElement(By.id("delayed")).click();
+		try {
+			browser.findElement(By.id("wait-if"));
+			fail("Element found");
+		} catch (NoSuchElementException e) {
+			assertNotNull(waitOn(browser, 6, SECONDS).untilAdded(By.id("wait-if")));
+		}
+	}
+
+	@Test
+	public void testUntilRemoved() {
+		browser.get(SharedWebDriverTest.TEST_PAGE);
+		browser.findElement(By.id("async")).click();
+		browser.findElement(By.id("wait-if"));
+		assertNotNull(waitOn(browser, 6000).untilRemoved(By.id("wait-if")));
+		assertEquals("1", browser.findElement(By.id("counter")).getText());
+	}
+
+	@Test
+	public void testUntilShown() {
+		browser.get(SharedWebDriverTest.TEST_PAGE);
+		browser.findElement(By.id("delayed")).click();
+		assertFalse(browser.findElement(By.id("wait-show")).isDisplayed());
+		assertTrue(waitOn(browser, 6, SECONDS).untilShown(By.id("wait-show")).isDisplayed());
+	}
+
+	@Test
+	public void testUntilVisible() {
+		browser.get(SharedWebDriverTest.TEST_PAGE);
+		browser.findElement(By.id("delayed")).click();
+		try {
+			browser.findElement(By.id("wait-if"));
+			fail("Element found");
+		} catch (NoSuchElementException e) {
+			assertTrue(waitOn(browser, 6000).untilShown(By.id("wait-if")).isDisplayed());
+		}
+	}
+
+	@Test
+	public void testUntilHidden() {
+		browser.get(SharedWebDriverTest.TEST_PAGE);
+		browser.findElement(By.id("async")).click();
+		browser.findElement(By.id("wait-show"));
+		assertFalse(waitOn(browser, 6, SECONDS).untilHidden(By.id("wait-show")).isDisplayed());
+	}
+
+	@Test
+	public void testUntilEnabled() {
+		browser.get(SharedWebDriverTest.TEST_PAGE);
+		browser.findElement(By.id("delayed")).click();
+		assertFalse(browser.findElement(By.id("wait-enable")).isEnabled());
+		assertTrue(waitOn(browser, 6000).untilEnabled(By.id("wait-enable")).isEnabled());
+	}
+
+	@Test
+	public void testUntilDisabled() {
+		browser.get(SharedWebDriverTest.TEST_PAGE);
+		browser.findElement(By.id("async")).click();
+		assertTrue(browser.findElement(By.id("wait-enable")).isEnabled());
+		waitOn(browser, 6, SECONDS).untilDisabled(By.id("wait-enable"));
+		assertEquals("1", browser.findElement(By.id("counter")).getText());
+	}
+}
