964e16e628a11113d2cd5e6636ebc54774675334
Roberto Lo Giacco <rlogiacco@gmail.com>
2015-10-15 22:26:56 +0200

    added missing method

--- src/main/java/org/agileware/test/web/AbstractDelegatingWebDriver.java
+++ src/main/java/org/agileware/test/web/AbstractDelegatingWebDriver.java
@@ -207,42 +207,52 @@ public abstract class AbstractDelegatingWebDriver implements WebDriver, TakesScr
 			this.delegate = delegate;
 		}
 		
+		@Override
 		public void click() {
 			delegate.click();
 		}
 
+		@Override
 		public void submit() {
 			delegate.submit();
 		}
 
+		@Override
 		public void sendKeys(CharSequence... keysToSend) {
 			delegate.sendKeys(keysToSend);
 		}
 
+		@Override
 		public void clear() {
 			delegate.clear();
 		}
 
+		@Override
 		public String getTagName() {
 			return delegate.getTagName();
 		}
 
+		@Override
 		public String getAttribute(String name) {
 			return delegate.getAttribute(name);
 		}
 
+		@Override
 		public boolean isSelected() {
 			return delegate.isSelected();
 		}
 
+		@Override
 		public boolean isEnabled() {
 			return delegate.isEnabled();
 		}
 
+		@Override
 		public String getText() {
 			return delegate.getText();
 		}
 
+		@Override
 		public List<WebElement> findElements(By by) {
 			try {
 				return delegate.findElements(by);
@@ -252,6 +262,7 @@ public abstract class AbstractDelegatingWebDriver implements WebDriver, TakesScr
 			}
 		}
 
+		@Override
 		public WebElement findElement(By by) {
 			try {
 				return delegate.findElement(by);
@@ -261,20 +272,29 @@ public abstract class AbstractDelegatingWebDriver implements WebDriver, TakesScr
 			}
 		}
 
+		@Override
 		public boolean isDisplayed() {
 			return delegate.isDisplayed();
 		}
 
+		@Override
 		public Point getLocation() {
 			return delegate.getLocation();
 		}
 
+		@Override
 		public Dimension getSize() {
 			return delegate.getSize();
 		}
 
+		@Override
 		public String getCssValue(String propertyName) {
 			return delegate.getCssValue(propertyName);
+		}
+
+		@Override
+		public <X> X getScreenshotAs(OutputType<X> type) throws WebDriverException {
+			return delegate.getScreenshotAs(type);
 		}
 	}
 }
